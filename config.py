455df475fab7179778d7cfe65996d4e93a9c875e
PhongN18 <chelsea2009021808@gmail.com>
2024-09-19 17:58:58 +0700

    profile page + responsive login

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
     SECRET_KEY = f'\xa2\xe4(\xb5\xaa#\xd5\x84\x91[~CE%\x1b#Z\xd0\xda\x04<]3\xc0'
-    SQLALCHEMY_DATABASE_URI = 'mysql://root:@localhost/rapidrescue'
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:phong.ndh20091808@localhost:3300/rapidrescue'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
